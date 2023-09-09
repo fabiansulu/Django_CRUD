5845bf6a9a0fdbfec06cd7cb9a3ae5a80a1b1399
Fabian Sulu <fabiansulu@gmail.com>
2023-09-09 11:42:04 +0200

    delete record by id

--- website/views.py
+++ website/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records  = Record.objects.all()
+
     #check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +23,7 @@ def home(request):
             messages.success(request, "There was an error logged in, Please try again ... !")
             return redirect('home')
     else :
-        return render(request, 'home.html', {}) 
+        return render(request, 'home.html', {'records': records}) 
 
 #def login_user(request):
  #   pass
@@ -38,13 +41,34 @@ def register_user(request):
             #Authenticate and login
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            user = authenticate(username =username, password=password)
+            user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, "You have successfully registered... welcome")
-        return redirect('home')
-    
+            return redirect('home')
     else : 
         form =SignUpForm()
         return render(request, 'register.html', {'form':form}) 
-    
     return render(request, 'register.html', {'form':form}) 
+
+def user_record(request, pk):
+    if request.user.is_authenticated: 
+        #look up records
+        user_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'user_record': user_record})
+    else :
+        messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated: 
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully...")
+        return redirect('home')
+    else :
+        messages.success(request, "You must be logged in to delete this record")
+        return redirect('home')
+
+def add_record(request):
+    return render(request, 'add_record.html', {})
+    
